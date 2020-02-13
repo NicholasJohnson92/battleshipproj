ea8b04bfedc172044539202d274c43ae06ae952d
Nick Johnson <daddeus72@gmail.com>
2020-02-13 11:50:55 -0600

    init

---
+++ Ship.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+  abstract  class Ship
+    {
+        public string name;
+        public int spaceSize;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
